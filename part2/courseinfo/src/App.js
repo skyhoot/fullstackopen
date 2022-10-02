7018c7af8cbe42dc80de9af32270f47e5605857f
edijsberzins <edijsberzins@protonmail.com>
2022-10-02 12:50:12 +0100

    complete exercises 2.2 & 2.3

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -12,10 +12,18 @@ const Content = ({ parts }) =>
     )}   
   </>
 
+const Total = ({ parts }) => {
+  const sum = parts.reduce((previousValue, currentValue) => previousValue + currentValue.exercises, 0)
+  return (
+    <p>total of {sum} exercises</p>
+  )
+}
+
 const Course = (props) =>
   <>
     <Header course={props.course.name} />
     <Content parts={props.course.parts} />
+    <Total parts={props.course.parts}/>
   </>
 
 const App = () => {
