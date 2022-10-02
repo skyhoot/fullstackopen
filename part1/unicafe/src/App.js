14dc7e034104ff98721467fff4436eae682fa85c
edijsberzins <edijsberzins@protonmail.com>
2022-10-02 05:45:13 +0100

    complete exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,7 +2,10 @@ import { useState } from 'react'
 
 const StatisticLine = (props) => {
   return (
-    <p>{props.text} {props.value}</p>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
@@ -11,12 +14,16 @@ const Statistics = (props) => {
     return (
       <div>
         <h1>statistics</h1>
-        <StatisticLine text='good' value={props.good} />
-        <StatisticLine text='neutral' value={props.neutral} />
-        <StatisticLine text='bad' value={props.bad} />
-        <StatisticLine text='all' value={props.all} />
-        <StatisticLine text='average' value={props.average} />
-        <StatisticLine text='positive' value={`${props.positive}%`} />
+        <table>
+          <tbody>
+            <StatisticLine text='good' value={props.good} />
+            <StatisticLine text='neutral' value={props.neutral} />
+            <StatisticLine text='bad' value={props.bad} />
+            <StatisticLine text='all' value={props.all} />
+            <StatisticLine text='average' value={props.average} />
+            <StatisticLine text='positive' value={`${props.positive}%`} />
+          </tbody>
+        </table>
       </div>
     )
   } else {
