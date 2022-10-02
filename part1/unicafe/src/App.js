b0e3ffe35360a08a73bdb8d868d493444a620825
edijsberzins <edijsberzins@protonmail.com>
2022-10-02 05:07:25 +0100

    complete exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,6 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + neutral + bad
+  const average = (good * 1 + bad * -1) / all
+  const positive = good * (100 / all)
 
   const handleClick = (props) => {
     if (props === 'good') {
@@ -28,6 +31,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
