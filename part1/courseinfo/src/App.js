34381ffb2dcf8d53fa2b310b4c7320db2bc0e184
edijsberzins <edijsberzins@protonmail.com>
2022-10-02 02:43:45 +0100

    fix whitespace

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,30 +1,30 @@
 const Header = (props) => {
   return (
-      <h1>{props.course}</h1>
+    <h1>{props.course}</h1>
   )
 }
 
 const Part = (props) => {
   return (
     <p>
-        {props.part} {props.exercises}
+      {props.part} {props.exercises}
     </p>
   )
 }
 
 const Content = (props) => {
   return (
-  <div>
+    <div>
       <Part part={props.part1} exercises={props.exercises1} />
       <Part part={props.part2} exercises={props.exercises2} />
       <Part part={props.part3} exercises={props.exercises3} />
-  </div>
+    </div>
   )
 }
 
 const Total = (props) => {
   return (
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   )
 }
 
